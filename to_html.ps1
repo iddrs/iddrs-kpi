c3ceb2e60cad1cb2b1f0e3019ba414bae2cb7426
Everton da Rosa <everton3x@gmail.com>
2023-06-22 15:24:47 -0300

    Novos testes de implantação no gh-pages

--- to_html.ps1
+++ to_html.ps1
@@ -2,5 +2,8 @@ Remove-Item -Path .\docs\* -Recurse -Force
 
 Copy-Item -Path .\_build\html\* -Destination .\docs\ -Recurse
 
+# Conforme orientação de https://jupyterbook.org/en/stable/publish/gh-pages.html
+New-Item -Path .\.nojekyll -ItemType File
+
 Write-Host "Pressione ENTER para finalizar:"
 Read-Host
