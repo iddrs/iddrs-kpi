d028ee5167a3bba8ec8896cb7c9c618f7da52a1d
Everton da Rosa <everton3x@gmail.com>
2023-06-22 15:28:05 -0300

    Ajuste do local de .nojekyll

--- to_html.ps1
+++ to_html.ps1
@@ -3,7 +3,7 @@ Remove-Item -Path .\docs\* -Recurse -Force
 Copy-Item -Path .\_build\html\* -Destination .\docs\ -Recurse
 
 # Conforme orientação de https://jupyterbook.org/en/stable/publish/gh-pages.html
-New-Item -Path .\.nojekyll -ItemType File
+New-Item -Path .\docs\.nojekyll -ItemType File
 
 Write-Host "Pressione ENTER para finalizar:"
 Read-Host
